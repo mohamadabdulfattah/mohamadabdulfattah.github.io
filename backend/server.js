1281e4b31001d40f6ea535a1cbefe58f3cd01de1
mohamadabdulfattah <abdulfattahmhamad@gmail.com>
2025-10-06 10:41:13 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/admin', adminRoutes);
 
 // Serve HTML pages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/projects', (req, res) => {
