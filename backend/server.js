01dc74c8992bda32a5a5e5423e4da3b83ff8aa53
mohamadabdulfattah <abdulfattahmhamad@gmail.com>
2025-10-06 10:44:27 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.use((req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on https://mohamadabdulfattah.github.io/:${PORT}`);
 });
 
 module.exports = app;
